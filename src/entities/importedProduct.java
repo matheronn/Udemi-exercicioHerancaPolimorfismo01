f203f5558c7f72dc0d6bbc564e262ddeb1eb47b4
matheronn <matheusem94@gmail.com>
2022-01-06 22:30:07 -0300

    Full Program

---
+++ src/entities/importedProduct.java
@@ -0,0 +1,37 @@
+package entities;
+
+public class importedProduct extends Product{
+	
+	private Double customsFee;
+		
+	public importedProduct() {
+		super();
+	}
+	
+	public importedProduct(String name, Double price, Double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+
+	public Double getCustomsFee() {
+		return customsFee;
+	}
+
+	public void setCustomsFee(Double customsFee) {
+		this.customsFee = customsFee;
+	}
+
+	public Double totalPrice() {
+		return super.getPrice() + customsFee;
+	}
+	
+	@Override
+	public String priceTag() {
+		return getName() 
+				+ " $ " 
+				+ String.format("%.2f", totalPrice())
+				+ " (Customs fee: $ " 
+				+ String.format("%.2f", customsFee)
+				+ ")";
+	}
+}
